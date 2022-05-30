745050fddfb27d6d72fb8eeefe70e45d42021869
tsnels <81826313+tsnels@users.noreply.github.com>
2022-05-30 09:35:42 +0200

    Initial commit

---
+++ src/test/java/be/vdab/fietsen/FietsenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.fietsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FietsenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
